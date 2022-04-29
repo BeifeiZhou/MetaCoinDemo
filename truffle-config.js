c302f1bc6cb331b717f23c3bce776af5d72e1198
Beifei Zhou <z.beifei@gmail.com>
2022-04-29 17:30:40 +0100

    Add environment variables

---
+++ truffle-config.js
@@ -0,0 +1,64 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+require('dotenv').config()
+
+const WALLET_KEY = process.env.WALLET_KEY
+const ROPSTEN_INFURA_API_KEY = process.env.ROPSTEN_INFURA_API_KEY
+const API_KEY_ETHERSCAN = process.env.API_KEY_ETHERSCAN
+
+module.exports = {
+  // Uncommenting the defaults below
+  // provides for an easier quick-start with Ganache.
+  // You can also follow this format for other networks.
+  // See details at: https://trufflesuite.com/docs/truffle/reference/configuration
+  // on how to specify configuration options!
+  //
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
+    },
+    ropsten: {
+      provider: function () {
+        return new HDWalletProvider(WALLET_KEY, `https://ropsten.infura.io/v3/${ROPSTEN_INFURA_API_KEY}`)
+      },
+      network_id: 3,
+      gas: 4000000      //make sure this gas allocation isn't over 4M, which is the max
+    }
+    //  test: {
+    //    host: "127.0.0.1",
+    //    port: 7545,
+    //    network_id: "*"
+    //  }
+    //},
+    //
+    // Truffle DB is currently disabled by default; to enable it, change enabled:
+    // false to enabled: true. The default storage location can also be
+    // overridden by specifying the adapter settings, as shown in the commented code below.
+    //
+    // NOTE: It is not possible to migrate your contracts to truffle DB and you should
+    // make a backup of your artifacts to a safe location before enabling this feature.
+    //
+    // After you backed up your artifacts you can utilize db by running migrate as follows:
+    // $ truffle migrate --reset --compile-all
+    //
+    // db: {
+    // enabled: false,
+    // host: "127.0.0.1",
+    // adapter: {
+    //   name: "sqlite",
+    //   settings: {
+    //     directory: ".db"
+    //   }
+    // }
+  },
+  compilers: {
+    solc: {
+      version: "^0.8.0"
+    }
+  },
+  plugins: ['truffle-plugin-verify'],
+  api_keys: {
+    etherscan: API_KEY_ETHERSCAN
+  }
+};
